b44d5ef2eb541c1e0cc3f7f553b972042c9125ae
H00Hong <144078099+H00Hong@users.noreply.github.com>
2024-11-14 15:24:45 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=C0413
-#   C0413: Import should be placed at the top of the module
 """
 A simple plotting library for the wxPython Phoenix project.
 
@@ -9,13 +7,11 @@ __version__ = "0.0.2"
 __updated__ = "2024-11-11"
 __docformat__ = "restructuredtext en"
 
-# For those who still use ``from package import *`` for some reason
 __all__ = [
     'PolyLine', 'PolySpline', 'PolyMarker', 'PolyBoxPlot', 'PolyHistogram',
     'BoxPlot', 'PolyBoxPlot', 'PlotGraphics', 'PlotCanvas', 'PlotPrintout'
 ]
 
-# Expose items so that the old API can still be used.
 from .plotcanvas import PlotCanvas
 
 from .polyobjects import (PolyPoints, PolyLine, PolySpline, PolyMarker,
@@ -23,6 +19,3 @@ from .polyobjects import (PolyPoints, PolyLine, PolySpline, PolyMarker,
                           PlotGraphics, PlotPrintout)
 
 from .utils import TempStyle, pendingDeprecation, PlotPendingDeprecation
-
-# For backwards compat.
-# BoxPlot = PolyBoxPlot
