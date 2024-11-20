b8c2280d5cc363b114eaa9b174b72a2638e77a39
H00Hong <1819642087@qq.com>
2024-11-20 10:46:09 +0800

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 ﻿from setuptools import setup, find_packages
 
-
+# python setup.py bdist_wheel
 setup(
     name='wxplot',  # 你的包名
     version='0.0.2',  # 版本号
     author='Yifan Hong',  # 作者名字
-    author_email='1819642087@qq.com',  # 作者邮箱
+    author_email='hong.yf@qq.com',  # 作者邮箱
     description='Some column improvements for `wx.lib.plot`',  # 简短描述
     long_description=open('README.md').read(),  # 长描述，通常是README的内容
     long_description_content_type='text/markdown',  # 长描述的内容类型
